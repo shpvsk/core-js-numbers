e6100298e5fde8676fde7102a8f2173310078474
shpvsk <mshpvsk@yandex.ru>
2024-11-25 21:31:38 +0300

    feat: add solution getAngleBetweenVectors

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -105,8 +105,13 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const dividend = x1 * x2 + y1 * y2;
+
+  const divisor1 = Math.sqrt(x1 * x1 + y1 * y1);
+  const divisor2 = Math.sqrt(x2 * x2 + y2 * y2);
+
+  return Math.acos(dividend / (divisor1 * divisor2));
 }
 
 /**
